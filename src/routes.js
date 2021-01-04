53853381735748e663269764507900bfe11ab8ea
GusBedasi <ggustavobedasi@gmail.com>
2021-01-04 17:50:16 -0300

    cleaned all comments

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,6 @@ const { Router } = require('express')
 const UserFactor = require('./factories/userFactory')
 const userService = UserFactor.generateInstance()
 
-//const UserService = require('./services/UserServices')
-//const userService = new UserService()
-
 const routes = Router()
 
 routes.post('/create/user', userService.createUser)
