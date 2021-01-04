53e723b399ee45187597f7f7f8b3908c34295112
GusBedasi <ggustavobedasi@gmail.com>
2021-01-04 17:47:13 -0300

    feat: add route to create user and database connection

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express')
 
-const userController = require('./factory/UserFactory')
+const UserFactor = require('./factories/userFactory')
+const userService = UserFactor.generateInstance()
+
+//const UserService = require('./services/UserServices')
+//const userService = new UserService()
 
 const routes = Router()
 
-routes.post('/create/user', userController.createUser)
+routes.post('/create/user', userService.createUser)
 
 module.exports = routes
